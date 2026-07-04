1c8619ae4b468d9be683af4eaa07ebef27874adc
potencialcarbanksp1-coder <potencialcarbank.sp1@gmail.com>
2026-07-03 22:10:55 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/producaoxpotencial/',
+  base: '/filial-sp1/',
   build: {
     outDir: 'dist'
   }
