2142574ab7e19359c59c0ed1216eb0c27b567ac5
potencialcarbanksp1-coder <potencialcarbank.sp1@gmail.com>
2026-07-06 22:34:18 -0300

    Update PaginaPainel.jsx

--- src/pages/PaginaPainel.jsx
+++ src/pages/PaginaPainel.jsx
@@ -1,15 +1,19 @@
 import TabelaPainel from '../components/TabelaPainel.jsx'
+import DashboardGcm from '../components/DashboardGcm.jsx'
 
 export default function PaginaPainel({ linhas, metaMeses, salvarMeta, alternarLmConsig, alternarNovaArea }) {
   return (
-    <div className="bloco bloco-tabela-principal">
-      <TabelaPainel
-        linhas={linhas}
-        metaMeses={metaMeses}
-        salvarMeta={salvarMeta}
-        alternarLmConsig={alternarLmConsig}
-        alternarNovaArea={alternarNovaArea}
-      />
-    </div>
+    <>
+      <DashboardGcm linhas={linhas} />
+      <div className="bloco bloco-tabela-principal">
+        <TabelaPainel
+          linhas={linhas}
+          metaMeses={metaMeses}
+          salvarMeta={salvarMeta}
+          alternarLmConsig={alternarLmConsig}
+          alternarNovaArea={alternarNovaArea}
+        />
+      </div>
+    </>
   )
 }
