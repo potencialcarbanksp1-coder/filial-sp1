7d0a8174f54d490b03eb8a6a7757841308228408
potencialcarbanksp1-coder <potencialcarbank.sp1@gmail.com>
2026-07-06 22:10:36 -0300

    Update PaginaPainel.jsx

--- src/pages/PaginaPainel.jsx
+++ src/pages/PaginaPainel.jsx
@@ -1,13 +1,11 @@
 import TabelaPainel from '../components/TabelaPainel.jsx'
 
-export default function PaginaPainel({ linhas, metaMeses, filtrosColuna, definirFiltroColuna, salvarMeta, alternarLmConsig, alternarNovaArea }) {
+export default function PaginaPainel({ linhas, metaMeses, salvarMeta, alternarLmConsig, alternarNovaArea }) {
   return (
     <div className="bloco bloco-tabela-principal">
       <TabelaPainel
         linhas={linhas}
         metaMeses={metaMeses}
-        filtrosColuna={filtrosColuna}
-        definirFiltroColuna={definirFiltroColuna}
         salvarMeta={salvarMeta}
         alternarLmConsig={alternarLmConsig}
         alternarNovaArea={alternarNovaArea}
